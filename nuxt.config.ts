0371dfe88a2959fc099bb921b0142ca23603fba8
Tim Hämisch <haemtim@gmail.com>
2024-02-05 23:52:43 +0100

    env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      clientID: process.env.NUXT_PUBLIC_CLIENT_ID, // NUXT_PUBLIC_CLIENT_ID
-      redirectURI: process.env.NUXT_PUBLIC_REDIRECT_URI, // NUXT_PUBLIC_REDIRECT_URI
-      serverURL: process.env.NUXT_PUBLIC_SERVER_URL, // NUXT_PUBLIC_SERVER_URL
+      clientID:
+        process.env.NUXT_PUBLIC_CLIENT_ID || "5b562fe7d96648f9ab6d2c72fbb7a06a", // NUXT_PUBLIC_CLIENT_ID
+      redirectURI:
+        process.env.NUXT_PUBLIC_REDIRECT_URI || "https://gts.thaemisch.com/auth", // NUXT_PUBLIC_REDIRECT_URI
+      serverURL:
+        process.env.NUXT_PUBLIC_SERVER_URL || "https://gts.thaemisch.com:3000", // NUXT_PUBLIC_SERVER_URL
     },
   },
   app: {
